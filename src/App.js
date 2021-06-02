e47e05598010824182c2358c265f0379c1974924
javierPerez <javip107@gmail.com>
2021-06-02 10:11:19 +0200

    N2- Add styles to the Joke Container component.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import JokeContainer from './components/JokeContainer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import CardsWeather from './components/weatherCards/CardsWeather';
 
 function App() {
   return (
     <div className="App">
       <JokeContainer />
+      {/* <CardsWeather /> */}
+      
     </div>
   );
 }
--- src/components/JokeContainer.js
+++ src/components/JokeContainer.js
@@ -1,21 +1,22 @@
-import axios from "axios";
-import {useState } from "react";
-import'../App.css'
-
+import axios from 'axios';
+import { useState } from 'react';
+import '../App.css';
 
 function JokeContainer() {
-  const [apiJoke, setApiJoke] = useState('')
-    const onClickChange = () => {
-    axios.get('https://api.chucknorris.io/jokes/random').then((apiData) =>{
-      console.log(apiData.data.value)
-      setApiJoke(()=>apiData.data.value)
-    })
+  const [apiJoke, setApiJoke] = useState('');
+  const onClickChange = () => {
+    axios.get('https://api.chucknorris.io/jokes/random').then((apiData) => {
+      console.log(apiData.data.value);
+      setApiJoke(() => apiData.data.value);
+    });
   };
   return (
-    <div className='JokeContainerCss'>
-        <h1>Preparat per riure? 🤣</h1>
-      <p className='Joke'>{apiJoke}</p>
-      <button onClick={onClickChange}>Següent acudit</button>
+    <div className="JokeContainerCss container">
+      <h1 className='JokeTitle'>Preparat per riure? 🤣</h1>
+      <p className="Joke">{apiJoke}</p>
+      <button className="btn btn-outline-info btn-lg btn-block w-50" onClick={onClickChange}>
+        Següent acudit
+      </button>
     </div>
   );
 }
